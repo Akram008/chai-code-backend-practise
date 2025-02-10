f05daaa1a12ca0a38fa0b4d1bd466c9efa756d24
Mohammad Akram <khanakram32392@gmail.com>
2025-02-10 17:55:21 +0530

    new changes

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+console.log(process.env)
+
+connectDB()
+
+
+
+/*
+import express from "express";
+const app = express()
+
+( async ()=> {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on('error', (error)=>{
+            console.log(`Errr: ${error}`)
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=> {
+            console.log(`App is listening at ${process.env.PORT}`)   
+        })
+
+    }catch(err){
+        console.log(`Error: ${err}`)
+        throw err;
+    }
+})()
+*/
